d383b457d8dc47b51524a66c54d0ffc045195167
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2023-11-30 15:37:07 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react-bootstrap/Col",
-        'react-bootstrap/Nav',
-        'react-bootstrap/Row',
-        'react-bootstrap/Tab'
-      ],
-    },
-  },
-  base: "/flashcard-app/"
+  plugins: [react()]
+  //base: "/flashcard-app/"
 })
